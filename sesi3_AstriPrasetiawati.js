7cc64e97a1704e1a4641cd3cce52b0db7d8bc500
astriprasetiawati <astri.prasetiawati24@gmail.com>
2025-12-10 11:17:24 +0700

    Add files via upload

---
+++ sesi3_AstriPrasetiawati.js
@@ -0,0 +1,20 @@
+//Mini Bootcamp Digital Sekola QA Engineer
+// Astri Prasetiawati
+// Tugas sesi 3 - Kamis, 4 Desember 2025
+
+
+let k = 1;
+let bintang = '*';
+do{
+  console.log (bintang) 
+  k++
+  bintang = bintang + '*'
+}while (k < 5)
+
+  console.log("==============================")
+
+let iconBintang = '*';
+for(i=1; i<=10; i++) {
+  console.log(iconBintang)
+  iconBintang = iconBintang + '*'
+}
